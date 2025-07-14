9651e1bf7c135a2d68b7fb9f20e452a3086fef44
Mathis Pinsault <pinsaultm@gmail.com>
2025-07-14 18:21:46 +0200

    feat(tanstack-router): add new plugin for TanStack Router integration
    - Introduced a new plugin for TanStack Router, including configuration files and TypeScript support.
    - Added dependencies for TanStack Router and related utilities in the package.json.
    - Updated workspace configuration to include the new plugin directory.
    - Enhanced documentation with schema definitions and project settings for the new plugin.

---
+++ plugins/configure/tanstack-router/src/index.ts
@@ -0,0 +1,63 @@
+import type { LoadPlugin } from "@impacts/types/plugins";
+import type { Runtime } from "@impacts/types/runtime";
+import { z } from "zod";
+
+const ecmaScriptOptionsSchema = z
+	.object({
+		/**
+		 *
+		 * @description Path to the routes folder
+		 */
+		routesPath: z.string().optional(),
+	})
+	.optional();
+
+type EcmaScriptOptions = z.infer<typeof ecmaScriptOptionsSchema>;
+
+import { tmpdir } from "node:os";
+import { Generator, getConfig } from "@tanstack/router-generator";
+import { Project } from "ts-morph";
+
+export async function tanstackRouter(
+	options: EcmaScriptOptions = {},
+): LoadPlugin {
+	return {
+		type: "load",
+		name: "tanstack-router",
+		async load(config, runtime: Runtime) {
+			const generator = new Generator({
+				root: process.cwd(),
+				config: getConfig({
+					routesDirectory: options.routesPath,
+					generatedRouteTree: `${tmpdir()}/impact-tree`,
+				}),
+			});
+
+			const out = {};
+
+			await generator.run();
+			const res = generator.getRoutesByFileMap();
+			const project = new Project();
+
+			for (const [key, value] of res.entries()) {
+				const sourceFile = project.addSourceFileAtPath(key);
+				const symbols = sourceFile.getExportSymbols();
+				const route = symbols.find((symbol) => symbol.getName() === "Route");
+				if (!route) {
+					out[value.routePath] = {
+						path: key,
+					};
+					continue;
+				}
+				const comments = route.getJsDocTags();
+				for (const comment of comments) {
+					console.log(comment.getName(), comment.getText());
+				}
+			}
+		},
+	};
+}
+
+export function createTanstackRouterPlugin(options: unknown) {
+	return tanstackRouter(tanstackRouterOptionsSchema.parse(options));
+}
